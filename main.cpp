1813889bd5b70fe618828626acc38c859519be4b
AmashOnBlitz <myselfamash@gmail.com>
2026-03-07 16:57:22 +0530

    Chore : Made base window for injector

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include "Windows.h"
+#include "tchar.h"
+#include "strsafe.h"
+
+LRESULT CALLBACK fnWinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow) {
+	TCHAR szAppName[] = _T("Auto Clicker");
+	TCHAR szMainWndName[] = _T("Minecraft Auto Clicker");
+	HWND hWnd = {};
+	WNDCLASS wndClass = {};
+	MSG msg = {};
+
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
+	wndClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = ::LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = fnWinProc;
+	wndClass.lpszClassName = szMainWndName;
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!::RegisterClass(&wndClass)) {
+		const int BuffLimit = 256;
+		TCHAR szBuff[BuffLimit];
+		DWORD DWErr = GetLastError();
+		StringCchPrintf(
+			szBuff,
+			BuffLimit,
+			_TEXT("Cannot Register Window Class Of Name '%s' \nReintalling The App May Fix!\nError Code: %lu"),
+			wndClass.lpszClassName,
+			DWErr
+		);
+		::MessageBox(NULL, szBuff, _TEXT("Internal Error"),MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
+	hWnd = ::CreateWindowEx(
+		WS_EX_OVERLAPPEDWINDOW,
+		szMainWndName,
+		szMainWndName,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		300,
+		350,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (hWnd == NULL) {
+		const int BuffLimit = 256;
+		TCHAR szBuff[BuffLimit];
+		DWORD DWErr = ::GetLastError();
+		StringCchPrintf(
+			szBuff,
+			BuffLimit,
+			_TEXT("Cannot Create Window Of Name '%s' \nReintalling The App May Fix!\nError Code: %lu"),
+			szMainWndName,
+			DWErr
+		);
+		::MessageBox(NULL, szBuff, _TEXT("Internal Error"), MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
+	::ShowWindow(hWnd, iCmdShow);
+	::UpdateWindow(hWnd);
+
+	while (::GetMessage(&msg, NULL, 0, 0)) {
+		::TranslateMessage(&msg);
+		::DispatchMessage(&msg);
+	}
+
+	return 1;
+}
+
+
+LRESULT CALLBACK fnWinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 1;
+	default:
+		break;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Minecraft Auto Clicker
+This project makes a dll and injector to inject dll into minecraft
+and the dll is auto clicker which triggers upon the assigned key and 
+auto clicks with unequal intervals (to mimic human and avoid being banned)
+  
+> Note : This project is under development (made for learning) 
+
+> Disclaimer : The author does not bear any responsibility of misuse of this tool
+or getting banned due to using this, as it is just for learning purposes!
